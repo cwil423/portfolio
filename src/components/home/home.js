3216ae16bb42ba901e9e1afa632cd512a2f8971e
Cole Williamson <williamsoncole@gmail.com>
2021-02-06 16:17:11 -0600

    Added homepage and sass.

---
+++ src/components/home/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './home.scss'
+
+export default function Home() {
+  return (
+
+    <div className='root'>
+      <div className="bg-image"></div>
+      <div className='title'>
+        <h1>
+          Cole Williamson
+        </h1>
+        <h2>Application Developer</h2>
+      </div>
+      
+
+    </div>
+
+
+    
+  )
+}
