2cf3e41e0f2c54e1f482e5e1cf233ba17ead4891
Cole Williamson <williamsoncole@gmail.com>
2021-02-10 17:23:20 -0600

    Added animated sun to home page.

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Home.scss';
+import sun from '../../images/sun.png';
 
 export default function Home() {
   return (
     <div className="home-root">
       <div className="title">
-        <h1>Hi, my name is Cole Williamson.</h1>
+        <h1>
+          Hi, my name is <h4>Cole Williamson.</h4>
+        </h1>
       </div>
+      <img className="image" src={sun} />
       <div className="subtitle">
         <h2>I develop web applications for you.</h2>
       </div>
